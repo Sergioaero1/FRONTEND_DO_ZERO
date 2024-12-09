0ecd14625a4eef33ed1a9ca391b0b8ac796618b9
Sergioaero <116237665+Sergioaero1@users.noreply.github.com>
2024-12-09 16:50:04 -0300

    upgrade
    css3 upgrade

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,7 @@ const menuLinks = document.querySelectorAll(".menu__link");
 function changeTheme(){
   const currentTheme = rootHtml.getAttribute("data-theme");
 
-  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
   toggleTheme.classList.toggle("bi-sun")
   toggleTheme.classList.toggle("bi-moon-stars")
@@ -20,12 +20,11 @@ accordionHeaders.forEach(header => {
     const accordionActive = accordionItem.classList.contains("active");
 
     accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
-  })
-})
-
+  });
+});
 menuLinks.forEach(item => {
   item.addEventListener("click", () => {
     menuLinks.forEach(i => i.classList.remove("active"));
     item.classList.add("active");
-  })
-})
+  });
+});
